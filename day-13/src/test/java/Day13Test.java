39f157effd8d85e1062ff6096496260fe321675b
luschmar <90399580+luschmar@users.noreply.github.com>
2023-11-22 13:27:59 +0100

    🎉 Init all days

---
+++ day-13/src/test/java/Day13Test.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day13Test {
+	@ParameterizedTest
+	@AocFileSource(inputs = {
+			@AocInputMapping(input = "test.txt", solution = "-1"),
+			@AocInputMapping(input = "input.txt", solution = "-1")
+	})
+	void part1(Stream<String> input, String solution) {
+		assertEquals(solution, Long.toString(input.count()));
+	}
+
+	@ParameterizedTest
+	@AocFileSource(inputs = {
+			@AocInputMapping(input = "test.txt", solution = "-1"),
+			@AocInputMapping(input = "input.txt", solution = "-1")
+	})
+	void part2(Stream<String> input, String solution) {
+		assertEquals(solution, Long.toString(input.count()));
+	}
+}
