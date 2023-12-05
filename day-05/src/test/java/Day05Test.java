0a30ef8e04db0eda049d2858edc759dcd20035d9
luschmar <90399580+luschmar@users.noreply.github.com>
2023-12-05 12:41:34 +0100

    ✨ efficient solve day-05 - with reverse lookup

--- day-05/src/test/java/Day05Test.java
+++ day-05/src/test/java/Day05Test.java
@@ -11,7 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day05Test {
 	@ParameterizedTest
-	@AocFileSource(inputs = {@AocInputMapping(input = "test.txt", solution = "35"), @AocInputMapping(input = "input.txt", solution = "278755257")})
+	@AocFileSource(inputs = {
+			@AocInputMapping(input = "test.txt", solution = "35"),
+			@AocInputMapping(input = "input.txt", solution = "278755257")}
+	)
 	void part1(Stream<String> input, String solution) {
 		var lines = input.toList();
 
@@ -73,6 +76,16 @@ class Day05Test {
 			}
 			return number;
 		}
+
+		long reverseLookup(long number) {
+			for (var r : ranges) {
+				if (number >= r.destination() && number < r.destination() + r.length()) {
+					var distance = number - r.destination();
+					return r.source() + distance;
+				}
+			}
+			return number;
+		}
 	}
 
 	record Range(long source, long destination, long length) {
@@ -81,7 +94,7 @@ class Day05Test {
 	@ParameterizedTest
 	@AocFileSource(inputs = {
 			@AocInputMapping(input = "test.txt", solution = "46"),
-			//@AocInputMapping(input = "input.txt", solution = "26829166") // runs 5min
+			@AocInputMapping(input = "input.txt", solution = "26829166") // runs 5min
 	})
 	void part2(Stream<String> input, String solution) {
 		var lines = input.toList();
@@ -93,7 +106,7 @@ class Day05Test {
 				var from = l.split(" ")[0].split("-")[0];
 				var to = l.split(" ")[0].split("-")[2];
 				currentTransformer = new Transformer(from, to);
-				transformers.put(from, currentTransformer);
+				transformers.put(to, currentTransformer);
 			} else {
 				var range = l.split(" ");
 				if (range.length == 3) {
@@ -107,18 +120,33 @@ class Day05Test {
 
 		AtomicReference<Long> minLocation = new AtomicReference<>(Long.MAX_VALUE);
 
-		toTest.forEach(t -> {
-			var currentTrans = transformers.get("seed");
-			var currentValue = t;
+		List<SRange> sRanges = IntStream.range(0, noPair.size() / 2).mapToObj(i -> new SRange(noPair.get(i * 2), noPair.get(i * 2 + 1))).toList();
+
+		for(long i = 0; i < Long.MAX_VALUE; i++) {
+			var currentTrans = transformers.get("location");
+			var currentValue = i;
 			while (currentTrans != null) {
-				currentValue = currentTrans.lookup(currentValue);
-				currentTrans = transformers.get(currentTrans.to);
+				currentValue = currentTrans.reverseLookup(currentValue);
+				currentTrans = transformers.get(currentTrans.from);
 			}
-			if (minLocation.get() > currentValue) {
-				minLocation.set(currentValue);
+
+			boolean hit = false;
+			for(var r : sRanges) {
+				if(currentValue >= r.start() && currentValue < r.start()+r.lenght()) {
+					hit = true;
+					minLocation.set(i);
+					break;
+				}
 			}
-		});
+			if(hit) {
+				break;
+			}
+		}
 
 		assertEquals(Long.parseLong(solution), minLocation.get());
+	}
+
+	record SRange(long start, long lenght) {
+
 	}
 }
