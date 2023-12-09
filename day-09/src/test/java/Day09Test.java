ea47b30f8a40f6f69f36eb2ead93d7d0217e5bdf
luschmar <90399580+luschmar@users.noreply.github.com>
2023-12-09 09:55:03 +0100

    🎨 refactor in streams

--- day-09/src/test/java/Day09Test.java
+++ day-09/src/test/java/Day09Test.java
@@ -1,60 +1,59 @@
 import org.junit.jupiter.params.ParameterizedTest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day09Test {
 	@ParameterizedTest
-	@AocFileSource(inputs = {
+	@AocFileSource(inputs = { 
 			@AocInputMapping(input = "test.txt", solution = "114"),
-			@AocInputMapping(input = "input.txt", solution = "1969958987")
-	})
+			@AocInputMapping(input = "input.txt", solution = "1969958987") 
+			})
 	void part1(Stream<String> input, String solution) {
 		var result = input.map(this::readLine).mapToInt(this::predictNextNumber).sum();
-		
-		
+
 		assertEquals(Integer.parseInt(solution), result);
 	}
-	
+
 	List<Integer> readLine(String l) {
-		return Arrays.stream(l.split(" "))
-				.map(Integer::parseInt).toList();
+		return Arrays.stream(l.split(" ")).map(Integer::parseInt).toList();
 	}
-	
+
 	int predictNextNumber(List<Integer> list) {
-		if(list.stream().allMatch(k -> k == 0)) {
+		if (list.stream().allMatch(k -> k == 0)) {
 			return 0;
 		}
-		var nextLevel = new ArrayList<Integer>();
-		for(int i = 0; i < list.size()-1;i++) {
-			nextLevel.add(list.get(i+1) - list.get(i));
-		}		
-		return list.get(list.size()-1) + predictNextNumber(nextLevel);
+		var nextLevel = IntStream.range(0, list.size() - 1)
+				.map(i -> list.get(i + 1) - list.get(i))
+				.boxed().toList();
+
+		return list.get(list.size() - 1) + predictNextNumber(nextLevel);
 	}
 
 	@ParameterizedTest
-	@AocFileSource(inputs = {
+	@AocFileSource(inputs = { 
 			@AocInputMapping(input = "test.txt", solution = "2"),
 			@AocInputMapping(input = "input.txt", solution = "1068")
-	})
+			})
 	void part2(Stream<String> input, String solution) {
 		var result = input.map(this::readLine).mapToInt(this::predictFirstNumber).sum();
 		assertEquals(Integer.parseInt(solution), result);
 
 	}
-	
+
 	int predictFirstNumber(List<Integer> list) {
-		if(list.stream().allMatch(k -> k == 0)) {
+		if (list.stream().allMatch(k -> k == 0)) {
 			return 0;
 		}
-		var nextLevel = new ArrayList<Integer>();
-		for(int i = 0; i < list.size()-1;i++) {
-			nextLevel.add(list.get(i+1) - list.get(i));
-		}		
+
+		var nextLevel = IntStream.range(0, list.size() - 1)
+				.map(i -> list.get(i + 1) - list.get(i))
+				.boxed().toList();
+
 		return list.get(0) - predictFirstNumber(nextLevel);
 	}
 }
