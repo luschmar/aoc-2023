cef2d5ca3cdb857337ad6ab14f506394b47b4d74
luschmar <90399580+luschmar@users.noreply.github.com>
2023-12-13 13:48:15 +0100

    ⚡️ improve day-13

--- day-13/src/test/java/Day13Test.java
+++ day-13/src/test/java/Day13Test.java
@@ -2,11 +2,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,8 +20,8 @@ class Day13Test {
 		var tables = new ArrayList<List<String>>();
 		var currentTable = new ArrayList<String>();
 		tables.add(currentTable);
-		for(var l : all) {
-			if("".equals(l)) {
+		for (var l : all) {
+			if ("".equals(l)) {
 				currentTable = new ArrayList<>();
 				tables.add(currentTable);
 				continue;
@@ -31,45 +29,38 @@ class Day13Test {
 			currentTable.add(l);
 		}
 
-		var sumH = tables.stream().mapToInt(this::findMirror).sum()*100;
+		var sumH = tables.stream().mapToInt(this::findMirror).sum() * 100;
 		var sumV = tables.stream().map(this::transpose).mapToInt(this::findMirror).sum();
 
-		var result = sumH+sumV;
-
+		var result = sumH + sumV;
 
 		assertEquals(Integer.parseInt(solution), result);
 	}
 
-	private List<String> transpose(List<String> t) {
+	List<String> transpose(List<String> t) {
 		var newTable = new ArrayList<String>();
-		IntStream.range(0, t.getFirst().length()).forEach(i -> newTable.add(t.stream().map(s -> Character.toString(s.charAt(i))).collect(Collectors.joining()))); ;
+		IntStream.range(0, t.getFirst().length()).forEach(i -> newTable.add(t.stream().map(s -> Character.toString(s.charAt(i))).collect(Collectors.joining())));
 		return newTable;
 	}
 
-	private int findMirror(List<String> t) {
-		var horizontal = IntStream.range(0, t.size()-1).filter(i -> t.get(i).equals(t.get(i+1)))
-				.filter(c -> {
+	int findMirror(List<String> t) {
+		return IntStream.range(0, t.size() - 1)
+				// prefilter candidates
+				.filter(i -> t.get(i).equals(t.get(i + 1))).map(c -> {
 					// mirror candidate; check rest
-					int mirrorSize = Integer.min(c+1, t.size()-c-1);
+					int mirrorSize = Integer.min(c + 1, t.size() - c - 1);
 
-					var res = IntStream.range(0, mirrorSize).allMatch(m -> t.get(c-m).equals(t.get(c+1+m)));
-					if(res) {
-						printTable(t);
-						System.out.println(" -> "+c+" mirrorsize "+mirrorSize);
+					var res = IntStream.range(0, mirrorSize).allMatch(m -> t.get(c - m).equals(t.get(c + 1 + m)));
+					if (res) {
+						//printTable(t);
+						//System.out.println(" -> " + c + " mirrorsize " + mirrorSize);
+						return c + 1;
 					}
-					return res;
-				}).map(cc -> cc+1).boxed().toList();
-		if(horizontal.size() > 1) {
-			System.err.println("Oh no...");
-		}
-		if(horizontal.isEmpty()){
-			return 0;
-		}
-
-		return horizontal.stream().mapToInt(a -> a).min().getAsInt();
+					return 0;
+				}).sum();
 	}
 
-	private void printTable(List<String> t) {
+	void printTable(List<String> t) {
 		System.out.println();
 		t.forEach(System.out::println);
 		System.out.println();
@@ -86,8 +77,8 @@ class Day13Test {
 		var tables = new ArrayList<List<String>>();
 		var currentTable = new ArrayList<String>();
 		tables.add(currentTable);
-		for(var l : all) {
-			if("".equals(l)) {
+		for (var l : all) {
+			if ("".equals(l)) {
 				currentTable = new ArrayList<>();
 				tables.add(currentTable);
 				continue;
@@ -102,68 +93,35 @@ class Day13Test {
 			var s1 = findSmudges(t1);
 			var s2 = findSmudges(t2);
 
-			if(s1.isEmpty()) {
-				return s2.getFirst().c();
+			if (s1 == 0) {
+				return s2;
 			}
-			return s1.getFirst().c() * 100;
+			return s1 * 100;
 		}).sum();
 
 		assertEquals(Integer.parseInt(solution), result);
 	}
 
-	private List<String> findSmudgeAndRepair(List<String> t) {
-		var smudges = findSmudges(t);
-		if(smudges.isEmpty()) {
-			System.err.println("Oh no Smudge");
-			return List.of();
-		}
-		if(smudges.size() > 1) {
-			System.err.println("Oh no multiple Smudge");
-		}
-		var s = smudges.getFirst();
-		var newTable = new ArrayList<String>();
+	int findSmudges(List<String> t) {
+		return IntStream.range(0, t.size() - 1).map(c -> {
+			// mirror candidate; check rest
+			int mirrorSize = Integer.min(c + 1, t.size() - c - 1);
 
-		for(int i = 0; i < t.size(); i++) {
-			if(i == s.x()) {
-				var b = new StringBuilder(t.get(i));
-				var replace = b.charAt(s.y()) == '#' ? '.' : '#';
-				b.replace(s.y(), s.y()+1, Character.toString(replace));
-				newTable.add(b.toString());
-			} else {
-				newTable.add(t.get(i));
+			int error = 0;
+			for (int i = 0; (error <= 1) && i < mirrorSize; i++) {
+				var up = t.get(c - i);
+				var down = t.get(c + 1 + i);
+				if (up.equals(down)) {
+					continue;
+				}
+				error += (int) IntStream.range(0, up.length()).filter(e -> up.charAt(e) != down.charAt(e)).count();
 			}
-		}
-		return newTable;
-	}
 
-	private List<Smudge> findSmudges(List<String> t) {
-		return IntStream.range(0, t.size()-1)
-				.mapToObj(c -> {
-					// mirror candidate; check rest
-					int mirrorSize = Integer.min(c+1, t.size()-c-1);
-
-					int error = 0;
-					int errorLine = 0;
-					int errorLocation = 0;
-					for(int i = 0; (error <= 1) && i < mirrorSize; i++) {
-						errorLine = c-i;
-						var up = t.get(c-i);
-						var down = t.get(c+1+i);
-						if(up.equals(down)) {
-							continue;
-						}
-						error += (int) IntStream.range(0, up.length()).filter(e -> up.charAt(e) != down.charAt(e)).count();
-						errorLocation = IntStream.range(0, up.length()).filter(e -> up.charAt(e) != down.charAt(e)).findFirst().getAsInt();
-					}
-
-					if(error == 1) {
-						System.out.println("ERROR: "+(errorLine)+":"+errorLocation+" -> "+c);
-						return new Smudge(errorLine, errorLocation, c+1);
-					}
-					return null;
-				}).filter(Objects::nonNull).toList();
-	}
-
-	record Smudge(int x, int y, int c) {
+			if (error == 1) {
+				// found mirror-axis with exact 1 error
+				return c + 1;
+			}
+			return 0;
+		}).sum();
 	}
 }
